bc8ec5fbb0469334e1c567ba776e03a2ce1411eb
Tawdi <ahmedtawdi00@gmail.com>
2025-04-17 22:56:27 +0100

    Migration | ajoute Reservation migration

---
+++ database/migrations/2025_04_17_194620_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('seance_id')->constrained('seances')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        // Reservation_Seat (pivot)
+        Schema::create('reservation_seat', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->constrained()->onDelete('cascade');
+            $table->foreignId('seat_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+        Schema::dropIfExists('reservation_seat');
+    }
+};
