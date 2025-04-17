84bd3802f326c63555161f6e674d6830e8f1fd8a
Tawdi <ahmedtawdi00@gmail.com>
2025-04-17 10:26:20 +0100

    Service | ajoute AuthService ( login , register)

---
+++ app/Services/AuthService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthService
+{
+    public function register(array $data): array
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'role' => $data['role'],
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return compact('user', 'token');
+    }
+
+    public function login(array $credentials): ?string
+    {
+        return auth('api')->attempt($credentials);
+    }
+}
