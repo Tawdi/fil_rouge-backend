e439aa84b1fc74feac8ad42da9e0ebce1c70cb11
Tawdi <ahmedtawdi00@gmail.com>
2025-04-17 10:43:40 +0100

    Controller | ajoute register fonction au AuthController (service)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Services\AuthService;
+
 class AuthController extends Controller
 {
+    protected AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $result = $this->authService->register($request->validated());
+        return response()->json($result);
+    }
+
 }
+
