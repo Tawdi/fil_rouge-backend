a72270162253fc8c4a65de1c7a1db413b5cf3ff1
Tawdi <ahmedtawdi00@gmail.com>
2025-04-17 23:16:23 +0100

    Controller | ajoute de Genre controller

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,48 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreGenreRequest;
+use App\Http\Requests\UpdateGenreRequest;
 use App\Models\Genre;
+use App\Services\GenreService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
+    public function __construct(protected GenreService $genreService) {}
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $genres = $this->genreService->all();
+        return response()->json($genres);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreGenreRequest $request): JsonResponse
     {
-        //
+        $genre = $this->genreService->create($request->validated());
+        return response()->json($genre, 201);
     }
-
     /**
      * Display the specified resource.
      */
-    public function show(Genre $genre)
+    public function show(Genre $genre): JsonResponse
     {
-        //
+        return response()->json($genre);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Genre $genre)
+    public function update(UpdateGenreRequest $request, Genre $genre): JsonResponse
     {
-        //
+        $genre = $this->genreService->update($genre, $request->validated());
+        return response()->json($genre);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Genre $genre)
+    public function destroy(Genre $genre): JsonResponse
     {
-        //
+        $this->genreService->delete($genre);
+        return response()->json(null, 204);
     }
 }
