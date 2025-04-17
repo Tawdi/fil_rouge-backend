00821dd6cd70a16954b918bca9e21c9893f554e7
Tawdi <ahmedtawdi00@gmail.com>
2025-04-17 22:55:08 +0100

    Migration | ajoute Seat and SeatType migration

---
+++ database/migrations/2025_04_17_194030_create_seats_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seat_types',function (Blueprint $table){
+            $table->id();
+            $table->string('name'); 
+        });
+        
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seat_type_id')->constrained()->onDelete('cascade');
+            $table->integer('number');
+            $table->integer('row'); // A,B,C ..
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('room_seat_type', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->foreignId('seat_type_id')->constrained()->onDelete('cascade');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seat_types');
+        Schema::dropIfExists('seats');
+        Schema::dropIfExists('room_seat_type');
+    }
+};
